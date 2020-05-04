a400e29f8f755e96ad56b6c0bc85d724c0898471
Jommel Suril <jommelsuril@umindanao.edu.ph>
2020-05-04 15:41:42 +0800

    adding files

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+	define('DB_SERVER', 'localhost');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', '');
+	define('DB_NAME', 'php_crud');
+	
+	$link = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+	if ($link->connect_errno) {
+	    printf("Connect failed: %s\n", $link->connect_error);
+	exit();
+}
